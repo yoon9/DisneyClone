eb678d2126aad1c382f3ea22d3ed8f057b704cb2
yunsu <yunsu1502@naver.com>
2024-04-14 01:24:28 +0900

    Feat: 상단 nav컴포넌트 및 app background 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+import Nav from "./components/Nav"
+
+import "./App.css"
+import homeBackground from "../src/assets/images/home-background.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Nav />
+    </Container>
+  )
 }
 
-export default App;
+export default App
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url(${homeBackground}) center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`
